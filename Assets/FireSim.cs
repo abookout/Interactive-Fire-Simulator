3e9340666b2ef4b97484dcc20339993fc717ea7f
Adam Bookout <adambookout@gmail.com>
2023-02-23 14:53:37 -0800

    Added velocity buffer and rest of sph equations (not working!)

--- Assets/FireSim.cs
+++ Assets/FireSim.cs
@@ -7,25 +7,27 @@ public class Main : MonoBehaviour
 {
     // This should match with the SPH CS!
     const int ThreadGroupSize = 64;
+    const int MaxNumParticles = 256;
 
     // Buffer and parameter IDs
     static readonly int
         particlePositionInputBufID = Shader.PropertyToID("_ParticlePositionInputBuf"),
+        particleVelocityInputBufID = Shader.PropertyToID("_ParticleVelocityInputBuf"), 
         particleAccelOutputBufID = Shader.PropertyToID("_ParticleAccelOutputBuf"),
 
-        maxNumParticlesID = Shader.PropertyToID("_MaxNumParticles"),
+        numParticlesID = Shader.PropertyToID("_NumParticles"),
         particleMassID = Shader.PropertyToID("_ParticleMass"),
         pressureStiffnessID = Shader.PropertyToID("_PressureStiffness"),
         referenceDensityID = Shader.PropertyToID("_ReferenceDensity"),
-        gravityID = Shader.PropertyToID("_Gravity");
+        extAccelerationsID = Shader.PropertyToID("_ExternalAccelerations");
 
     [Header("Set in inspector")]
     [SerializeField] new ParticleSystem particleSystem;
-    [SerializeField] int maxNumParticles = 32;
+    [SerializeField, Range(1, MaxNumParticles)] int numParticles = 32;
     [SerializeField] float particleMass = 0.1f;
     [SerializeField] float pressureStiffness = 1f;
     [SerializeField] float referenceDensity = 1f;      // Probably choose atmostpheric pressure
-    [SerializeField] float gravityStrength = 9.8f;
+    [SerializeField] Vector3 externalAccelerations = new(0, -9.8f, 0);      // Just gravity, for now at least
 
     // Main compute shader for calulating update to particle accelerations
     [Header("Buffer & shader object properties")]
@@ -35,8 +37,9 @@ public class Main : MonoBehaviour
     [SerializeField] RenderTexture tex;
     //[SerializeField] Texture3D tex;
 
-    // Particle positions are given to the compute shader as an input
+    // Particle positions and current velocities are given to the compute shader as an input
     [SerializeField] ComputeBuffer particlePositionInputBuf;
+    [SerializeField] ComputeBuffer particleVelocityInputBuf;
     // Buffer for the calculation results
     [SerializeField] ComputeBuffer particleAccelOutputBuf;
 
@@ -69,14 +72,13 @@ public class Main : MonoBehaviour
     //    Graphics.Blit(tex, destination);
     //}
 
-
     void InitializeBuffers()
     {
         if (tex == null)
         {
-            tex = new RenderTexture(maxNumParticles, maxNumParticles, 0, RenderTextureFormat.ARGBFloat);
+            tex = new RenderTexture(numParticles, numParticles, 0, RenderTextureFormat.ARGBFloat);
             tex.dimension = UnityEngine.Rendering.TextureDimension.Tex3D;
-            tex.volumeDepth = maxNumParticles;
+            tex.volumeDepth = numParticles;
             tex.enableRandomWrite = true;
             tex.wrapMode = TextureWrapMode.Clamp;
             tex.filterMode = FilterMode.Point;
@@ -88,19 +90,57 @@ public class Main : MonoBehaviour
             //tex.Apply();
         }
 
-        int mainID = SPHComputeShader.FindKernel("CSComputeAccel");
+        //int mainID = SPHComputeShader.FindKernel("CSComputeAccel");
 
         // TODO: is there a better way of computing this?
         int sizeofVec3 = sizeof(float) * 3;
 
         // Define the particle position buffer as structured, with dynamic, unsynchronized access
-        particlePositionInputBuf = new ComputeBuffer(maxNumParticles, sizeofVec3, ComputeBufferType.Default, ComputeBufferMode.SubUpdates);
-        particleAccelOutputBuf = new ComputeBuffer(maxNumParticles, sizeofVec3, ComputeBufferType.Default, ComputeBufferMode.SubUpdates);
+        particlePositionInputBuf = new ComputeBuffer(numParticles, sizeofVec3, ComputeBufferType.Default, ComputeBufferMode.SubUpdates);
+        particleVelocityInputBuf = new ComputeBuffer(numParticles, sizeofVec3, ComputeBufferType.Default, ComputeBufferMode.SubUpdates);
+        particleAccelOutputBuf = new ComputeBuffer(numParticles, sizeofVec3, ComputeBufferType.Default, ComputeBufferMode.SubUpdates);
 
-
-        //TODO: write constants buffer and maybe also positions buffer here?
+        //TODO: write constants and maybe also positions buffer here?
     }
-    
+
+    void ReleaseBuffers()
+    {
+        particlePositionInputBuf.Release();
+        particleVelocityInputBuf.Release();
+        particleAccelOutputBuf.Release();
+    }
+
+    // Needs the buffers to have been initialized first.
+    // At first, just instantiates the max number of particles on load, randomly distributed within a cube.
+    void SetupParticleSystem()
+    {
+        ParticleSystem.Particle[] particles = new ParticleSystem.Particle[numParticles];
+        // This is how you set particle system struct b/c it's a struct that exposes the underlying implementation
+        var main = particleSystem.main;
+        main.maxParticles = numParticles;
+
+        //var shape = particleSystem.shape;
+        particleSystem.Emit(numParticles);
+
+        // Populate particles array from system
+        particleSystem.GetParticles(particles);
+
+        NativeArray<Vector3> GPUPosArray = particlePositionInputBuf.BeginWrite<Vector3>(0, numParticles);
+        NativeArray<Vector3> GPUVelArray = particleVelocityInputBuf.BeginWrite<Vector3>(0, numParticles);
+        for (int i = 0; i < numParticles; i++)
+        {
+            GPUPosArray[i] = particles[i].position;
+            GPUVelArray[i] = particles[i].velocity;
+        }
+        particleVelocityInputBuf.EndWrite<Vector3>(numParticles);
+        particlePositionInputBuf.EndWrite<Vector3>(numParticles);
+
+        // TODO: Set position and scale of a bounding box for the fire to exist for the sake of keeping the fire localized (??)
+
+        //particleSystem.SetParticles(particles, maxNumParticles);
+    }
+
+
     // Dispatch a job to the GPU to run a new SPH simulation step
     void UpdateParticleAccelerations()
     {
@@ -109,16 +149,19 @@ public class Main : MonoBehaviour
         // Set debug property because it blows up if you don't
         SPHComputeShader.SetTexture(mainID, Shader.PropertyToID("Result"), tex);
 
+        // Set buffers
         SPHComputeShader.SetBuffer(mainID, particlePositionInputBufID, particlePositionInputBuf);
+        SPHComputeShader.SetBuffer(mainID, particleVelocityInputBufID, particleVelocityInputBuf);
         SPHComputeShader.SetBuffer(mainID, particleAccelOutputBufID, particleAccelOutputBuf);
 
         // Set constants
-        SPHComputeShader.SetInt(maxNumParticlesID, maxNumParticles);
+        SPHComputeShader.SetInt(numParticlesID, numParticles);
         SPHComputeShader.SetFloat(particleMassID, particleMass);
         SPHComputeShader.SetFloat(pressureStiffnessID, pressureStiffness);
         SPHComputeShader.SetFloat(referenceDensityID, referenceDensity);
-        SPHComputeShader.SetFloat(gravityID, gravityStrength);
-        //  Make sure there's at least one thread group!
+        SPHComputeShader.SetVector(extAccelerationsID, externalAccelerations);
+
+        //TODO: update particle velocities each dispatch!
 
 
         // This works with [numthreads(8, 8, 1)]
@@ -137,41 +180,15 @@ public class Main : MonoBehaviour
         //SPHComputeShader.Dispatch(0, tex.width * 16, 1, 1);
 
         // Squared num particles because of the size of the test texture
-        int numThreadGroups = Mathf.Max(1, maxNumParticles * maxNumParticles * maxNumParticles / ThreadGroupSize);
+        //  Make sure there's at least one thread group!
+        int numThreadGroups = Mathf.Max(1, numParticles * numParticles * numParticles / ThreadGroupSize);
 
         SPHComputeShader.Dispatch(mainID, numThreadGroups, 1, 1);
-    }
 
-    void ReleaseBuffers()
-    {
-        particlePositionInputBuf.Release();
-        particleAccelOutputBuf.Release();
-    }
-
-    // Needs the buffers to have been initialized first
-    void SetupParticleSystem()
-    {
-        ParticleSystem.Particle[] particles = new ParticleSystem.Particle[maxNumParticles];
-        //particles = new ParticleSystem.Particle[maxNumParticles];
-        // This is how you set particle system struct b/c it's a struct that exposes the underlying implementation
-        var main = particleSystem.main;
-        main.maxParticles = maxNumParticles;
-
-        //var shape = particleSystem.shape;
-        particleSystem.Emit(maxNumParticles);
-
-        // Populate particles array from system
-        particleSystem.GetParticles(particles);
-
-        NativeArray<Vector3> GPUArray = particlePositionInputBuf.BeginWrite<Vector3>(0, maxNumParticles);
-        for (int i = 0; i < maxNumParticles; i++)
-        {
-            GPUArray[i] = particles[i].position;
-        }
-        particlePositionInputBuf.EndWrite<Vector3>(maxNumParticles);
-
-        // TODO: Set position and scale of a bounding box for the fire to exist for the sake of keeping the fire localized (??)
-
-        //particleSystem.SetParticles(particles, maxNumParticles);
+        // Now, accels should be populated
+        Vector3[] test = new Vector3[numParticles];
+        particleAccelOutputBuf.GetData(test);
+        
+        Debug.Log(string.Join(", ", test));
     }
 }
